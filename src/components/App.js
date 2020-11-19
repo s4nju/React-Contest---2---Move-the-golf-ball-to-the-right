671b6dc3f2b1f3eaa486dfbe738f5b280ac6421a
sanju.io <email@example.com>
2020-11-19 21:05:55 +0530

    ball is moving

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,37 @@ class App extends Component {
         super(props)
         this.state = {
             renderBall: false,//variable needed to be changed
-            posi : 0,
+            posi: 0,
             ballPosition: { left: "0px" }
         };
         this.renderChoice = this.renderChoice.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.arrowKeyHandler = this.arrowKeyHandler.bind(this)
     };
 
     //call back function
     buttonClickHandler() {
-   
-   }
+        this.setState({renderBall: true})
+    }
     renderChoice() {
-		if(this.state.renderBall){return <div className="ball" style={this.state.ballPosition}></div>}
-		 else   return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+        if (this.state.renderBall) { return <div className="ball" style={this.state.ballPosition}></div> }
+        else return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+    }
+
+    arrowKeyHandler(event) {
+        if (event.key === "ArrowRight" && event.keyCode === 39) {
+            let bp = parseInt(this.state.ballPosition.left) + 5 + "px";
+            this.setState({ballPosition: {left: bp}})
+        }
     }
 
     //bind ArrowRight keydown event
     componentDidMount() {
-      
+        window.addEventListener('keydown', this.arrowKeyHandler);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.arrowKeyHandler);
     }
 
     render() {
